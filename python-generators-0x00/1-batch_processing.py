33452ff675ef85f7fb4073dafb4f227d2ba28090
Otieno Onyango Kelvin <kelvinonyango155@gmail.com>
2025-05-14 22:13:40 -0700

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,1 +1,36 @@
+import mysql.connector
+import os
 
+def stream_users_in_batches(batch_size):
+    """Yield batches of users"""
+    conn = mysql.connector.connect(
+        host="localhost",
+        user=os.getenv('DB_USER', 'root'),
+        password=os.getenv('DB_PASSWORD', ''),
+        database="ALX_prodev"
+    )
+    offset = 0
+    
+    while True:
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute(
+            "SELECT * FROM user_data LIMIT %s OFFSET %s",
+            (batch_size, offset)
+        )
+        batch = cursor.fetchall()
+        cursor.close()
+        
+        if not batch:
+            break
+            
+        yield batch
+        offset += batch_size
+    
+    conn.close()
+
+def batch_processing(batch_size):
+    """Process batches to filter users > 25"""
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user['age'] > 25:
+                print(user)
