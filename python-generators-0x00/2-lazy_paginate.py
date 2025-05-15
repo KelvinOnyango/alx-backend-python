4376208035db41663b1eb807ac1f0f6ded3cd235
Otieno Onyango Kelvin <kelvinonyango155@gmail.com>
2025-05-14 22:14:49 -0700

    Update 2-lazy_paginate.py

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -1,1 +1,11 @@
+import seed
 
+def lazy_paginate(page_size):
+    """Lazily load paginated data"""
+    offset = 0
+    while True:
+        page = seed.paginate_users(page_size, offset)
+        if not page:
+            break
+        yield page
+        offset += page_size
