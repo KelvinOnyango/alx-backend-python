bc163ef046100e61d567cb3d8bec9a97dd622b5a
Otieno Onyango Kelvin <kelvinonyango155@gmail.com>
2025-05-14 22:18:55 -0700

    Update 2-lazy_paginate.py

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -1,10 +1,29 @@
-import seed
+import mysql.connector
+import os
+
+def paginate_users(page_size, offset):
+    """Fetch a page of users from the database"""
+    conn = mysql.connector.connect(
+        host="localhost",
+        user=os.getenv('DB_USER', 'root'),
+        password=os.getenv('DB_PASSWORD', ''),
+        database="ALX_prodev"
+    )
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(
+        "SELECT * FROM user_data LIMIT %s OFFSET %s",
+        (page_size, offset)
+    )
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return rows
 
 def lazy_paginate(page_size):
-    """Lazily load paginated data"""
+    """Generator for lazy loading paginated data"""
     offset = 0
     while True:
-        page = seed.paginate_users(page_size, offset)
+        page = paginate_users(page_size, offset)
         if not page:
             break
         yield page
