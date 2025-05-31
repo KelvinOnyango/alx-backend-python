13155ebaab8d2fae854eca854b5c89065f4e938c
Otieno Onyango Kelvin <kelvinonyango155@gmail.com>
2025-05-31 08:59:35 -0700

    Update views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,45 +1,61 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
+from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
-from .models import Conversation, Message
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import User, Conversation, Message
 from .serializers import (
-    ConversationSerializer,
+    UserSerializer,
     MessageSerializer,
+    ConversationSerializer,
     ConversationCreateSerializer,
     MessageCreateSerializer
 )
 
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['online']
+    search_fields = ['username', 'email', 'first_name', 'last_name']
+    ordering_fields = ['username', 'last_seen']
+    ordering = ['username']
+
 class ConversationViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Conversation.objects.all()
-    
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['participants']
+    ordering_fields = ['updated_at']
+    ordering = ['-updated_at']
+
     def get_serializer_class(self):
         if self.action == 'create':
             return ConversationCreateSerializer
         return ConversationSerializer
-    
+
+    def get_queryset(self):
+        return self.queryset.filter(participants=self.request.user)
+
     def perform_create(self, serializer):
         conversation = serializer.save()
         conversation.participants.add(self.request.user)
-    
-    def get_queryset(self):
-        return self.queryset.filter(participants=self.request.user)
 
 class MessageViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Message.objects.all()
-    
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['conversation', 'sender', 'read']
+    ordering_fields = ['sent_at']
+    ordering = ['-sent_at']
+
     def get_serializer_class(self):
         if self.action == 'create':
             return MessageCreateSerializer
         return MessageSerializer
-    
+
     def get_queryset(self):
-        conversation_id = self.request.query_params.get('conversation')
-        if conversation_id:
-            return self.queryset.filter(conversation__id=conversation_id)
-        return self.queryset.none()
-    
+        return self.queryset.filter(conversation__participants=self.request.user)
+
     def perform_create(self, serializer):
         conversation_id = self.request.data.get('conversation')
         conversation = Conversation.objects.get(id=conversation_id)
