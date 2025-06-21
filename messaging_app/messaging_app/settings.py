2f527cdc8bf0c9e0fd1eabf67ca47a0cc4e69775
Kelvin Onyango <kelvinonyango155@gmail.com>
2025-06-21 09:19:55 -0700

    update

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -66,9 +66,13 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '3306',
     }
 }
 
